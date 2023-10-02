a6d63b3bd52df5237b9b746630507a250e821af7
tjmclain <thomas.mclain@gmail.com>
2023-10-02 16:04:38 -0700

    remove usings

--- Logger.cs
+++ Logger.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace Myna.Unity.Debug
