6280ad55e0d5ed0b765acdc646b4ba80e5d65b5c
tjmclain <thomas.mclain@gmail.com>
2023-10-02 13:18:54 -0700

    start writing loginfo class

---
+++ LogInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Myna.Unity.Debug
+{
+	using UnityObject = UnityEngine.Object;
+
+	public class LogInfo
+	{
+		private readonly List<object> _message = new List<object>(4);
+		private string _tag = string.Empty;
+		private UnityObject _context = null;
+		private bool _isValid = false;
+
+		public LogInfo Append(object message)
+		{
+			if (_isValid)
+			{
+				_message.Add(message);
+			}
+
+			return this;
+		}
+	}
+}
