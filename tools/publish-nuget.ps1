377ece4057b577da52f6e4a419cc02d34c6478f5
tjmclain <thomas.mclain@gmail.com>
2023-10-04 09:24:50 -0700

    update publish-nuget script

--- tools/publish-nuget.ps1
+++ tools/publish-nuget.ps1
@@ -1,4 +1,15 @@
+Write-Host ""
+Write-Host "Test Release configuration"
 dotnet test -c Release
+
+Write-Host ""
+Write-Host "Build Release configuration"
 dotnet build -c Release
+
+Write-Host ""
+Write-Host "Pack NuGet package"
 dotnet pack
-dotnet nuget push ".\bin\Release\*.nupkg" --source https://api.nuget.org/v3/index.json
+
+Write-Host ""
+Write-Host "Push Nuget package"
+dotnet nuget push ".\bin\Release\*.nupkg" --source https://api.nuget.org/v3/index.json --skip-duplicate
